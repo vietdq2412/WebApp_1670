d29bd227150cc4747b799d0218c8b93f30e12649
vietdq2412 <67895234+vietdq2412@users.noreply.github.com>
2022-03-31 10:35:20 +0700

    add book

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,9 +1,44 @@
-const express = require('express')
+const express = require('express');
+const async = require('hbs/lib/async');
 const router = express.Router()
-const { insertObject, checkUserRole } = require('../databaseHandler')
+const { insertObject, checkUserRole, search, PRODUCT_TABLE, CATEGORY_TABLE } = require('../databaseHandler')
 
-router.get('/add', (req, res) => {
-    res.render('product/addProductForm')
+router.get('/', async (req, res) => {
+    // if (req.session.username) {
+    //     username = req.session.username;
+    // } else {
+    //     res.redirect('/login');
+    // }
+
+    const products = await search('', PRODUCT_TABLE);
+
+    // if (req.session.username) {
+    //     username = req.session.username;
+    // }
+    console.log(products);
+    res.render('product/listProducts', { products: products})
+})
+
+router.get('/add', async (req, res) => {
+    const categories = await search('', CATEGORY_TABLE);
+    res.render('product/addProductForm', {categories:categories})
+})
+
+router.post('/add', (req, res) => {
+    const name = req.body.txtName;
+    const price = req.body.txtPrice;
+    const image = req.body.txtImage;
+    const category = req.body.txtCategory;
+
+
+    objectToInsert = {
+        name: name,
+        price: price,
+        image:image,
+        category:category
+    }
+    insertObject(PRODUCT_TABLE, objectToInsert);
+    res.redirect('/product')
 })
 
 module.exports = router;
