85c21338f5306e71368c91dbbaf2bf723efb6e3f
kientt123 <kienttgch190451@fpt.edu.vn>
2022-04-14 08:27:18 +0700

    push

--- src/app.js
+++ src/app.js
@@ -91,7 +91,10 @@ app.get('/product/edit',async(req,res)=>{
     console.log(document)
     res.render('edit',{product:document})
 
+})
 
+app.get('/checkout', (req,res) => {
+    res.render('checkout')
 })
 
 
