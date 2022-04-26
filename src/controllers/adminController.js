ea347359af7c48178b7fdc0bce4dd94eedaee8cb
vietdq2412 <67895234+vietdq2412@users.noreply.github.com>
2022-04-26 09:48:15 +0700

    phân quyền

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -2,10 +2,17 @@ const express = require('express')
 const router = express.Router()
 
 const path = require('path')
+const {getCurrentUserSession} = require("../databaseHandler");
 
 
 router.get('/', (req, res) => {
-    res.render('admin/adminPage')
+    const curUser = getCurrentUserSession(req, res);
+    if (!curUser) {
+        req.session.error = "please login first!";
+        res.redirect("/authen/login");
+        return;
+    }
+    res.render('admin/adminPage', {user:curUser})
 })
 
 module.exports = router;
